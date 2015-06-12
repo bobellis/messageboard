6230ccf53a3c41de3e75195a46a10be0a12b44bd
bobellis <bobellis.tx@gmail.com>
2015-06-12 14:02:41 -0700

    Add edit and save methods to question controller

--- controllers/question.js
+++ controllers/question.js
@@ -1,5 +1,12 @@
 Messageboard.QuestionController = Ember.ObjectController.extend({
+  isEditing: false,
   actions: {
+    edit: function(){
+      this.set('isEditing', true);
+    },
+    save: function() {
+      this.set('isEditing', false);
+    },
     delete: function(){
       if (confirm('Are you sure?')) {
         this.get('model').destroyRecord();
